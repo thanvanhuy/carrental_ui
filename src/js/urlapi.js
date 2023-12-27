f7b3eb1437ca393e37c551bcef7088a7958da417
Thân Văn Huy <92750807+thanvanhuy@users.noreply.github.com>
2023-12-27 13:04:09 +0700

    Add files via upload

--- src/js/urlapi.js
+++ src/js/urlapi.js
@@ -1,5 +1,5 @@
 //const urlapi = 'http://api.dev.kythuatvien.net:2020/api/';
-const urlapi = 'http://192.188.0.126:2020/api/';
+const urlapi = 'https://carrental-api.onrender.com/api/';
 module.exports = {
     urlapi
 }
